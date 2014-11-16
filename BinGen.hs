58167a77d876f498cdfdb9da3d28e58149836925
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-11-16 19:41:40 +0000

    Added assembling for 16bit ALU instructions

--- BinGen.hs
+++ BinGen.hs
@@ -20,7 +20,7 @@ getRegIncs :: [((Alu8Op, Register), Word8)]
 getRegIncs = zip (zip (repeat Inc) regs) [0x04, 0x0C, 0x14, 0x1C, 0x24, 0x2C, 0x3C] 
 
 getRegDecs :: [((Alu8Op, Register), Word8)]
-getRegDecs = zip (zip (repeat Inc) regs) [0x05, 0x0D, 0x15, 0x1D, 0x25, 0x2D, 0x3D] 
+getRegDecs = zip (zip (repeat Dec) regs) [0x05, 0x0D, 0x15, 0x1D, 0x25, 0x2D, 0x3D] 
 
 aluRegTable :: M.Map (Alu8Op, Register) Word8
 aluRegTable = M.fromList $
@@ -37,16 +37,47 @@ aluRegTable = M.fromList $
     
             
 
+aluWordTable :: M.Map Alu8Op Word8
+aluWordTable = M.fromList $ 
+    zip [Add, Adc, Sub, Sbc, And, Xor, Or, Cmp]
+        [0xC6, 0xCE, 0xD6, 0xDE, 0xE6, 0xEE, 0xF6, 0xFE]
+
+
+aluHLTable :: M.Map Alu8Op Word8
+aluHLTable = M.fromList $
+    zip [Add, Adc, Sub, Sbc, And, Xor, Or, Cmp, Inc, Dec]
+        [0x86, 0x8E, 0x96, 0x9E, 0xA6, 0xAE, 0xB6, 0xBE, 0x34, 0x35]
 
 -- |Generate Gameboy Binary code from Assembly
 --genCode :: Program -> B.ByteString
 --genCode (Program xs) = concatMap genStmt xs
 
 
-
---genStmt :: Stmt -> B.ByteString
---genStmt (Alu8 a) = genAlu8 a
-
+genStmt :: Stmt -> B.ByteString
+genStmt (Alu8 a) = genAlu8 a
+genStmt (Alu16 a) = genAlu16 a
 
 genAlu8 :: Alu8Stmt -> B.ByteString
-genAlu8 (AluReg op reg) = B.singleton $ M.findWithDefault 0x0 (op, reg) aluRegTable
+genAlu8 (AluReg op reg) = B.singleton $ M.findWithDefault 0xD3 (op, reg) aluRegTable
+genAlu8 (AluImm op word) = B.pack [M.findWithDefault 0xD3 op aluWordTable ,word] 
+genAlu8 (AluMemHl op) = B.singleton $ M.findWithDefault 0xD3 op aluHLTable  
+
+genAlu16 :: Alu16Stmt -> B.ByteString
+genAlu16 (Inc16 reg) = B.singleton $ case reg of
+    BC -> 0x03
+    DE -> 0x13
+    HL -> 0x23
+    SP -> 0x33
+genAlu16 (Dec16 reg) = B.singleton $ case reg of
+    BC -> 0x0B
+    DE -> 0x1B
+    HL -> 0x2B
+    SP -> 0x3B
+genAlu16 (AddSpIm w) = B.pack [0xE8, w]
+genAlu16 (AddHL reg) = B.singleton $ case reg of
+    BC -> 0x09
+    DE -> 0x19
+    HL -> 0x29
+    SP -> 0x39
+
+
--- InstructionTree.hs
+++ InstructionTree.hs
@@ -16,6 +16,8 @@ CombinedRegister(..),
 Alu8Op(..))
 where
 
+import Data.Word
+
 data Program = Program [Stmt] deriving (Show)
 
 data Stmt = Alu8 Alu8Stmt 
@@ -27,46 +29,46 @@ data Stmt = Alu8 Alu8Stmt
             deriving (Show, Eq, Ord)
 
 data Alu8Stmt = AluReg Alu8Op Register 
-              | AluImm Alu8Op Integer
+              | AluImm Alu8Op Word8
               | AluMemHl Alu8Op 
                deriving (Show,Eq, Ord)
 
 data Alu16Stmt = Inc16 CombinedRegister
                | Dec16 CombinedRegister
-               | AddSpIm Integer
+               | AddSpIm Word8
                | AddHL CombinedRegister
                deriving (Show,Eq, Ord)
 
 data Ld8Stmt = LdRegReg Register Register -- Load register contents into register
-            | LdRegIm  Register Integer -- Load immediate 8 bit unsigned val into Reg
+            | LdRegIm  Register Word8 -- Load immediate 8 bit unsigned val into Reg
             | LdRegMemHl Register  -- Load value at address HL into register
             | LdMemHlReg Register  -- Load register into memory at address HL
             | LdAMemCombinedReg CombinedRegister --Load value at address Combined reg into A
             | LdMemCombinedRegA CombinedRegister --Load contents of register A into mem address of reg
-            | LdAMemIm Integer --Load value at immediate 16 bit address into register A
+            | LdAMemIm Word16 --Load value at immediate 16 bit address into register A
             | LdiAmemHL
             | LdimemHLA
             | LddAmemHL
             | LddmemHLA
             | LDIOAC   
             | LDIOCA    
-            | LdhIOA Integer
-            | LdhAIO Integer 
+            | LdhIOA Word8
+            | LdhAIO Word8 
               deriving (Show,Eq, Ord)
 
-data Ld16Stmt = LdCombinedRegIm CombinedRegister Integer
-              |  LdMemSp Integer -- Load SP contents into given 16 bit address 
+data Ld16Stmt = LdCombinedRegIm CombinedRegister Word16
+              |  LdMemSp Word16 -- Load SP contents into given 16 bit address 
               |  LdSpHl -- Load contents of HL into SP
-              |  LdHLSpPlusIm Integer -- Load contents of memory 
+              |  LdHLSpPlusIm Word16 -- Load contents of memory 
               |  Push StackRegister -- Push contents of combined reg onto stack
               |  Pop StackRegister -- Pop contents of combined reg from stack
                  deriving (Show, Eq, Ord)
 
-data JmpStmt = JmpIm Integer -- Jump to 16 bit immediate address
-             | JmpCond Condition Integer -- Jump if Condition is met
+data JmpStmt = JmpIm Word16 -- Jump to 16 bit immediate address
+             | JmpCond Condition Word16 -- Jump if Condition is met
              | JmpHL -- Jump to address contained in register HL
-             | JmpRel Integer -- Jump to 8 bit address relative to PC
-             | JmpRelCond Condition Integer -- Relative Jump if Condition is met
+             | JmpRel Word8 -- Jump to 8 bit address relative to PC
+             | JmpRelCond Condition Word8 -- Relative Jump if Condition is met
                deriving (Show, Eq, Ord) 
 
 data MiscellaneousStmt = 
